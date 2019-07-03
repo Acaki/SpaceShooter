fb3f68f8739a897ed4357d6d47179ea7516e7122
Acaki <axsz4298@gmail.com>
2019-07-03 14:32:47 +0800

    Add background scrolling

---
+++ Assets/BgScroll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgScroll : MonoBehaviour
+{
+    public float scrollSpeed;
+    public float tileSize;
+
+    private Vector2 _startPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSize);
+        transform.position = _startPosition + Vector2.up * newPosition;
+    }
+}
