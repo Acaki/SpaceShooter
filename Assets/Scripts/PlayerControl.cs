bd55696bc0ae572ddab40eb96b31d11262dbd2c5
Acaki <axsz4298@gmail.com>
2019-07-12 13:26:39 +0800

    Update

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed;
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        GetComponent<Rigidbody2D>().velocity = movement * speed;
+    }
+}
