dfedaffa637d38b47074e711ceedf97c9e613d30
Acaki <axsz4298@gmail.com>
2019-07-12 13:57:32 +0800

    Movement

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,16 +1,34 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Schema;
 using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, yMin, yMax;
+}
 
 public class PlayerControl : MonoBehaviour
 {
     public float speed;
+    public Boundary boundary;
     private void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        GetComponent<Rigidbody2D>().velocity = movement * speed;
+        Rigidbody2D body = GetComponent<Rigidbody2D>();
+        body.velocity = movement * speed;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            body.velocity *= 0.5f;
+        }
+        body.position = new Vector2(
+            Mathf.Clamp(body.position.x, boundary.xMin, boundary.xMax),
+            Mathf.Clamp(body.position.y, boundary.yMin, boundary.yMax)
+        );
+        
     }
 }
