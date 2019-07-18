3b0a5ff2ce778e92731edba5741efcbbbfc8deb8
Acaki <axsz4298@gmail.com>
2019-07-18 13:24:17 +0800

    Destroy enemy

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
