b56c963094bfdbdc0e867a8e25d4015785010ba9
Acaki <axsz4298@gmail.com>
2019-08-08 17:55:06 +0800

    Fix sprite disappear

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour
 {
+    public GameObject explosion;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Boundary")
+        if (other.CompareTag("Boundary"))
         {
             return;
         }
+
+        Instantiate(explosion, transform);
         Destroy(other.gameObject);
         Destroy(gameObject);
     }
